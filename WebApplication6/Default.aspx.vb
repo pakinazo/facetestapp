4b7b56ad98125031b61bfec58eb70b5f33524380
pakinazo <johnpaga777@hotmail.com>
2013-12-20 15:47:27 -0600

    25

--- WebApplication6/Default.aspx.vb
+++ WebApplication6/Default.aspx.vb
@@ -22,7 +22,7 @@ Public Class _Default
                 'Session("inscripcionURLRegreso") = Request.Url.AbsoluteUri
 
                 'Dim myURL As String = Session("myurl")
-                Dim FbURL As String = String.Format("https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&state={2}&scope=user_birthday,email,user_hometown,publish_actions", "779337262082870", "https://apps.facebook.com/primercanvazazo", Session.SessionID)
+                Dim FbURL As String = String.Format("https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&state={2}&scope=user_birthday,email,user_hometown,publish_actions", "779337262082870", "https://pakinazocanvas.apphb.com/default.aspx", Session.SessionID)
 
                 Response.Redirect(FbURL)
             End If
@@ -33,7 +33,7 @@ Public Class _Default
    
     Private Sub getUserData(ByVal code As String)
         Dim MyURL As String = Session("myurl")
-        Dim fbURL As String = String.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", "779337262082870", "https://apps.facebook.com/primercanvazazo", "0e4c136ef9121b45a272c8d43e77509b", code)
+        Dim fbURL As String = String.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", "779337262082870", "https://pakinazocanvas.apphb.com/default.aspx", "0e4c136ef9121b45a272c8d43e77509b", code)
 
         Dim fbWeb As New WebClient
         Dim result As String = fbWeb.DownloadString(fbURL)
@@ -57,7 +57,7 @@ Public Class _Default
         LBpicture.Text = "<img src=""http://graph.facebook.com/" & usrRest.username & "/picture?type=large""/>"
         Dim code2 As String = Request("code")
         Dim state2 As String = Request("state")
-        Inscribete.Text = "<a href=""http://registro.tiempooficial.com/default.aspx?evento=26&facebook=on>¡Inscríbete!</a>"
+        Inscribete.Text = "<a href=""http://registro.tiempooficial.com/default.aspx?evento=26&facebook=on"">¡Inscríbete!</a>"
         'LBpicture.Text = "<img src=""" & usrRest.pic_big_with_logo & """/>"
     End Sub
 
