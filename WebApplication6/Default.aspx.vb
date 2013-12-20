f8c694400f20f5fd86c0ec46ebef99bece82633f
pakinazo <johnpaga777@hotmail.com>
2013-12-19 19:44:48 -0600

    8

--- WebApplication6/Default.aspx.vb
+++ WebApplication6/Default.aspx.vb
@@ -28,7 +28,7 @@ Public Class _Default
    
     Private Sub getUserData(ByVal code As String)
         Dim MyURL As String = Session("myurl")
-        Dim fbURL As String = String.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", "294444543985836", MyURL, "0757f6a496872d0d3c02087c183d0c2f", code)
+        Dim fbURL As String = String.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", "779337262082870", MyURL, "0e4c136ef9121b45a272c8d43e77509b", code)
 
         Dim fbWeb As New WebClient
         Dim result As String = fbWeb.DownloadString(fbURL)
@@ -47,11 +47,6 @@ Public Class _Default
         Dim fbClient As New Facebook.FacebookClient(access_token)
         Dim usrRest = fbClient.Get("me")
 
-        Try
-            My.Computer.FileSystem.WriteAllText("c://canvas/registroFB.txt", usrRest.ToString, True)
-        Catch ex As Exception
-
-        End Try
 
         LBNombre.Text = usrRest.first_name
         
