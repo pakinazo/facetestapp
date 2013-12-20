a9c8c95f719c91535fb04bed0dcc10861fd2f855
pakinazo <johnpaga777@hotmail.com>
2013-12-19 18:18:47 -0600

    4

--- WebApplication6/Default.aspx.designer.vb
+++ WebApplication6/Default.aspx.designer.vb
@@ -12,4 +12,13 @@ Option Explicit On
 
 
 Partial Public Class _Default
+
+    '''<summary>
+    '''LBNombre control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents LBNombre As Global.System.Web.UI.WebControls.Label
 End Class
--- WebApplication6/Default.aspx.vb
+++ WebApplication6/Default.aspx.vb
@@ -2,6 +2,9 @@
     Inherits Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
+        Dim access_token = "f24caa53a309a170aa35609011176b6b"
+        Dim fbClient As New Facebook.FacebookClient(access_token)
+        Dim usrRest = fbClient.Get("me")
+        LBNombre.Text = usrRest.first_name
     End Sub
 End Class
