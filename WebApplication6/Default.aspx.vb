8f78f2e328d88652760589db9a63434c9b67a10e
juan lopez <johnloco2005@hotmail.com>
2013-12-21 13:59:03 -0600

    house6

--- WebApplication6/Default.aspx.vb
+++ WebApplication6/Default.aspx.vb
@@ -42,7 +42,7 @@ Public Class _Default
 
     Private Sub getUserData(ByVal code As String)
         Dim MyURL As String = Session("myurl")
-        Dim fbURL As String = String.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", "779337262082870", "https://pakinazocanvas.apphb.com/default.aspx", "0e4c136ef9121b45a272c8d43e77509b", code)
+        Dim fbURL As String = String.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}", "779337262082870", "https://apps.facebook.com/primercanvazazo", "0e4c136ef9121b45a272c8d43e77509b", code)
 
         Dim fbWeb As New WebClient
         Dim result As String = fbWeb.DownloadString(fbURL)
