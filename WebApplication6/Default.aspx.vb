ce7fecbbb725264185bbb60f46c660d6e949e562
pakinazo <johnpaga777@hotmail.com>
2014-04-02 19:00:41 -0600

    first and lastnames

--- WebApplication6/Default.aspx.vb
+++ WebApplication6/Default.aspx.vb
@@ -87,14 +87,14 @@ Public Class _Default
 
         Try
             Dim resultado As Object = fbClient.Get("fql",
-          New With {.q = "SELECT uid, username FROM user " & _
+          New With {.q = "SELECT uid, username, first_name, last_name FROM user " & _
                          "WHERE is_app_user = 1 AND uid IN (SELECT uid2 FROM friend WHERE uid1 = me())"})
 
             Dim datos As String = Newtonsoft.Json.JsonConvert.SerializeObject(resultado)
             LabelDatosFace.Text += datos
             Dim facebookFriends As Friends = New JavaScriptSerializer().Deserialize(Of Friends)(datos)
             For Each item In facebookFriends.data
-                LabelDatosFace.Text += String.Format("id: {0}, name: {1}", item.uid, item.username)
+                LabelDatosFace.Text += String.Format("id: {0}, name: {1}", item.uid, item.username, item.first_name, item.last_name)
                 Console.WriteLine("id: {0}, name: {1}", item.uid, item.username)
             Next
 
--- WebApplication6/FacebookFriend.vb
+++ WebApplication6/FacebookFriend.vb
@@ -9,6 +9,7 @@
         End Set
     End Property
     Private m_uid As String
+
     Public Property username() As String
         Get
             Return m_username
@@ -18,4 +19,24 @@
         End Set
     End Property
     Private m_username As String
+
+    Public Property first_name() As String
+        Get
+            Return m_first_name
+        End Get
+        Set(value As String)
+            m_first_name = value
+        End Set
+    End Property
+    Private m_first_name As String
+
+    Public Property last_name() As String
+        Get
+            Return m_last_name
+        End Get
+        Set(value As String)
+            m_last_name = value
+        End Set
+    End Property
+    Private m_last_name As String
 End Class
