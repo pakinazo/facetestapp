54822584785def8e7410da557f7032a02f86bbd6
pakinazo <johnpaga777@hotmail.com>
2015-05-14 21:10:23 -0500

    testapp

--- WebApplication6/Share.aspx.vb
+++ WebApplication6/Share.aspx.vb
@@ -4,10 +4,10 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         
         Dim Etiqueta As String = "https://www.facebook.com/dialog/share?" & _
-                    "app_id=145634995501895" & _
+                    "app_id=546495888778745" & _
                     "&display=popup" & _
-                    "&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F" & _
-                    "&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer"
+                    "&href=http://www.lanacion.com.ar/edicion-impresa/suplementos/revista" & _
+                    "&redirect_uri=http://www.lanacion.com.ar/edicion-impresa/suplementos/revista"
         ResponseHelper.Redirect(Etiqueta, "_blank", "")
     End Sub
 
