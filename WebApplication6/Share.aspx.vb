309dc761da7a1cdbbc2a5369c224868c7f3aadcd
pakinazo <johnpaga777@hotmail.com>
2015-05-14 20:54:48 -0500

    share

--- WebApplication6/Share.aspx.vb
+++ WebApplication6/Share.aspx.vb
@@ -4,7 +4,7 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         
         Dim Etiqueta As String = "https://www.facebook.com/dialog/share?" & _
-                    "app_id = 145634995501895" & _
+                    "app_id=145634995501895" & _
                     "&display=popup" & _
                     "&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F" & _
                     "&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer"
