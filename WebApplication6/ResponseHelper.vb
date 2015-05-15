e0235ef1bb642dc3fd08e894dd9bb0d91a0d8062
pakinazo <johnpaga777@hotmail.com>
2015-05-14 20:51:53 -0500

    sharedialog

---
+++ WebApplication6/ResponseHelper.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Object
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.Control
+Public Class ResponseHelper
+    Private Sub New()
+    End Sub
+    Public Shared Sub Redirect(ByVal url As String, ByVal target As String, ByVal windowFeatures As String)
+        Dim context As HttpContext = HttpContext.Current
+        If (String.IsNullOrEmpty(target) OrElse target.Equals("_self", StringComparison.OrdinalIgnoreCase)) AndAlso String.IsNullOrEmpty(windowFeatures) Then
+            context.Response.Redirect(url)
+        Else
+            Dim page As Page = CType(context.Handler, Page)
+            If page Is Nothing Then
+                Throw New InvalidOperationException("Cannot redirect to new window outside Page context.")
+            End If
+            url = page.ResolveClientUrl(url)
+            Dim script As String
+            If (Not String.IsNullOrEmpty(windowFeatures)) Then
+                script = "window.open(""{0}"", ""{1}"", ""{2}"");"
+            Else
+                script = "window.open(""{0}"", ""{1}"");"
+            End If
+            script = String.Format(script, url, target, windowFeatures)
+            page.ClientScript.RegisterStartupScript(GetType(Page), "Redirect", script, True)
+        End If
+    End Sub
+End Class
---
+++ WebApplication6/Share.aspx.vb
@@ -0,0 +1,14 @@
+﻿Public Class Share
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        
+        Dim Etiqueta As String = "https://www.facebook.com/dialog/share?" & _
+                    "app_id = 145634995501895" & _
+                    "&display=popup" & _
+                    "&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F" & _
+                    "&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer"
+        ResponseHelper.Redirect(Etiqueta, "_blank", "")
+    End Sub
+
+End Class
