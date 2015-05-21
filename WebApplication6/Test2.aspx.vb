0a3687b75b57e7425a5e1fcaa892b8f30528e0ba
pakinazo <johnpaga777@hotmail.com>
2015-05-20 20:37:47 -0500

    aceptando

--- WebApplication6/Test2.aspx.vb
+++ WebApplication6/Test2.aspx.vb
@@ -70,7 +70,7 @@ Public Class Test2
             'href= … & 
             'template=You have people waiting to play with you, play now!
             Dim UrlNotificar As String = "http://pakinazocanvas.apphb.com/Test2.aspx"
-            Dim txtNotificación As String = "Buen día @[100002078392441] debido a cambios en facebook debes confirmar tu número de competidor en tu Tiempo Digital, disculpa las molestias"
+            Dim txtNotificación As String = "Buen día @[100002078392441] debido a cambios en facebook debes confirmar tu número de competidor en tu Tiempo Digital, aceptando publicar en tu muro"
             Dim idNotificar As String = "100002078392441"
     
 
