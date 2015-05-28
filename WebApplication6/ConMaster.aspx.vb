4c0bc1416e8c063042955e1e956deedde463dc88
pakinazo <johnpaga777@hotmail.com>
2015-05-27 21:00:09 -0500

    ADS

--- WebApplication6/ConMaster.aspx.vb
+++ WebApplication6/ConMaster.aspx.vb
@@ -7,13 +7,18 @@
 
     Private Sub CambiarEncabezadoOGFacebook()
         Try
+            Dim builder As UriBuilder = New UriBuilder(Request.Url.AbsoluteUri)
+            builder.Port = -1
+            Dim newUri As Uri = builder.Uri
+
             Dim imageTag As String = "<meta property=""og:image"" content=""http://pakinazocanvas.apphb.com/Images/mapa.png""/>"
             Dim descriptionTag As String = "<meta property=""og:description"" content=""Comparación Side By Side durante la competencia""/>"
-            Dim urlTag As String = String.Format("<meta property=""og:url"" content=""{0}""/>", Request.Url.ToString)
+            Dim urlTag As String = String.Format("<meta property=""og:url"" content=""{0}""/>", newUri)
             Dim typeTag As String = "<meta property=""og:type"" content=""article"" />"
             Dim app_idTag As String = "" ' String.Format("<meta property=""fb:app_id"" content=""{0}""/>", ConfigurationManager.AppSettings("FB_Client_ID"))
+            Dim titleTag As String = "<meta property=""og:title"" content=""PERIDOCAZAZO"" />"
 
-            Dim facebookTags = imageTag & descriptionTag & urlTag & typeTag & app_idTag
+            Dim facebookTags = imageTag & descriptionTag & urlTag & typeTag & app_idTag & titleTag
             DirectCast(Me.Page.Master.FindControl("OGFacebookMetaTags"), Literal).Text = facebookTags
         Catch ex As Exception
 
