5881bf7f48abba8df40e9cae296bd3b3be9fbfa1
pakinazo <johnpaga777@hotmail.com>
2015-05-27 20:44:28 -0500

    b

--- WebApplication6/Site.Master.designer.vb
+++ WebApplication6/Site.Master.designer.vb
@@ -14,6 +14,15 @@ Option Explicit On
 Partial Public Class SiteMaster
 
     '''<summary>
+    '''OGFacebookMetaTags control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents OGFacebookMetaTags As Global.System.Web.UI.WebControls.Literal
+
+    '''<summary>
     '''HeadContent control.
     '''</summary>
     '''<remarks>
